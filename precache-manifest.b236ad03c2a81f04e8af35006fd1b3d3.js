a7b5533907c083c220de39c6db2ad5e4329135b2
Caleb Habtegebriel <calebhabtegebriel@calebs-air.home>
2020-07-19 00:46:02 -0500

    Updates

---
+++ precache-manifest.b236ad03c2a81f04e8af35006fd1b3d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73b9a01290fe01fb2ae1b143ed151622",
+    "url": "/myportfoliosite/index.html"
+  },
+  {
+    "revision": "585a75c656b56a0a8cb3",
+    "url": "/myportfoliosite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "09a16f3ab5253d7ff6be",
+    "url": "/myportfoliosite/static/js/2.b94fef6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myportfoliosite/static/js/2.b94fef6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "585a75c656b56a0a8cb3",
+    "url": "/myportfoliosite/static/js/main.e27efdc1.chunk.js"
+  },
+  {
+    "revision": "a9c8716d10921dbee87d",
+    "url": "/myportfoliosite/static/js/runtime-main.5aa7204c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myportfoliosite/static/media/logo.5d5d9eef.svg"
+  }
+]);
